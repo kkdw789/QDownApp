aa5fd12cb65cfb07a7b50df0eca34a99e8ca2696
qinke <286556038@qq.com>
2016-12-08 18:05:51 +0800

    简单主界面

--- QDownApp/MainWindow.xaml.cs
+++ QDownApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QDownApp.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,32 @@ namespace QDownApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        TransportingWin transportingWin = new TransportingWin();
+        DustbinWin dustbinWin = new DustbinWin();
+        CompletedWin completedWin = new CompletedWin();
         public MainWindow()
         {
             InitializeComponent();
+            MainGrid.Children.Add(transportingWin);
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           if(ListMen.SelectedIndex==0)
+           {
+               MainGrid.Children.Clear();
+               MainGrid.Children.Add(transportingWin);
+           }
+           if (ListMen.SelectedIndex == 1)
+           {
+               MainGrid.Children.Clear();
+               MainGrid.Children.Add(dustbinWin);
+           } 
+            if (ListMen.SelectedIndex == 2)
+           {
+               MainGrid.Children.Clear();
+               MainGrid.Children.Add(completedWin);
+           }
         }
     }
 }
